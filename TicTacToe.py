8115aa7390d4566e0da3bd57c6ac728d99205a99
Wonkadude <darius.wigglesworth@gmail.com>
2020-02-09 23:05:57 -0500

    Initial commit, created basics

---
+++ TicTacToe.py
@@ -0,0 +1,13 @@
+humanWins = 0
+programWins = 0
+ties = 0
+
+print("Welcome to the unbeatable tic-tac-toe machine\nYou have won ", humanWins, " to the programs ", programWins, "wins with ", ties, " ties")
+print("Would you like to play first? Y/N")
+
+entered = input()
+if entered != 'Y' or entered != 'y'
+    print("Invalid input")
+    return -1
+
+
